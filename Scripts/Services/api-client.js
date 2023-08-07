0af01da436216af018eb552b2e5d9a89aed5acd3
Dhruvgarg91 <dhruv.g4445@gmail.com>
2023-08-07 18:06:39 +0530

    Updated

--- Scripts/Services/api-client.js
+++ Scripts/Services/api-client.js
@@ -1,7 +1,7 @@
 // Network Call Code
 export default async function doNetworkCall() {
   const URL =
-    "https://gist.githubusercontent.com/Dhruvgarg91/ff8347e5fded74a0d4578eeabf1a2f2b/raw/adda0168d688a753943a015f85022a4a3df1920d/Pizza.json";
+    "https://gist.githubusercontent.com/Dhruvgarg91/286ca53ba020c63ed5872f9c912f9aa0/raw/32a5e963851abb0217ac93fba6e048bf92a9d52b/gistfile1.txt";
   try {
     const response = await fetch(URL);
     const object = await response.json();
